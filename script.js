191b57b4baab867fd307f887dbd24eacd7a53e57
FayedAlMamun <fayedruet@gmail.com>
2020-07-30 04:27:22 +0600

    last update

--- script.js
+++ script.js
@@ -2,27 +2,32 @@ function generatePin(){
     let pin=Math.floor(Math.random()*(9999-1000+1))+1000;
     document.getElementById("display-pin").value=pin;
     document.getElementById("submit-btn").disabled=false;
+    document.getElementById("display-number").disabled=false; 
     document.getElementById("display-number").value="";
     document.getElementById("try-left").innerText=3;
     document.getElementById("try-left-text").style.display="block"
-    document.getElementById("pin-matched").style.display="none";
-    document.getElementById("wrong-pin").style.display="none";
-
+    notifySection();
 }
 function setOrClearNumber(numberId)
 {
-    if(numberId=="clear-all"){
+    if(numberId=="clear-all" && !isDisable()){
         document.getElementById("display-number").value="";
+        notifySection();
     }
-    else if(numberId=="backspace"){
+    else if(numberId=="backspace" && !isDisable()){
         let inputValue=document.getElementById("display-number").value;
         let newInputValue = inputValue.slice(0,-1);
         document.getElementById("display-number").value=newInputValue;
+        notifySection();
     }
     else{
-    let numberValue=document.getElementById(numberId).innerText;
-    let inputValue=document.getElementById("display-number").value;
-    document.getElementById("display-number").value=inputValue+ numberValue; 
+        if(!isDisable())
+        {
+            let numberValue=document.getElementById(numberId).innerText;
+            let inputValue=document.getElementById("display-number").value;
+            document.getElementById("display-number").value=inputValue+ numberValue; 
+            notifySection();
+        }
     }
 }
 function checkPinValidity(){
@@ -42,10 +47,24 @@ function checkPinValidity(){
             document.getElementById("pin-matched").style.display="none";
             document.getElementById("wrong-pin").style.display="block"
             document.getElementById("try-left-text").style.display="block"
-            document.getElementById("try-left").innerText=tryLeftValue-1;
+            tryLeftValue=tryLeftValue-1;
+            document.getElementById("try-left").innerText=tryLeftValue;
         }
     }
-    else if(tryLeftValue>0 && pin!=""){
+    if(tryLeftValue==0 && pin!=""){
         document.getElementById("submit-btn").disabled=true;
+        document.getElementById("display-number").disabled=true;   
     }
+}
+function notifySection()
+{
+    document.getElementById("pin-matched").style.display="none";
+    document.getElementById("wrong-pin").style.display="none";
+}
+function isDisable(){
+    if(document.getElementById("submit-btn").disabled==true)
+    {
+        return true;
+    }
+    return false;
 }
