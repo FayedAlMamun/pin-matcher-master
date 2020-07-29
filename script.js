f6cc9657dce13735dc0bf4818b85dab786aba51a
FayedAlMamun <fayedruet@gmail.com>
2020-07-29 17:36:23 +0600

    pin-macher completed

---
+++ script.js
@@ -0,0 +1,46 @@
+function generatePin(){
+    let pin=Math.floor(Math.random()*(9999-1000+1))+1000;
+    document.getElementById("display-pin").disabled=false;
+    document.getElementById("display-pin").value=pin;
+    document.getElementById("display-pin").disabled=true;
+}
+function setOrClearNumber(numberId)
+{
+    if(numberId=="clear-all"){
+        document.getElementById("display-number").value="";
+    }
+    else if(numberId=="backspace"){
+        let inputValue=document.getElementById("display-number").value;
+        let newInputValue = inputValue.slice(0,-1);
+        document.getElementById("display-number").value=newInputValue;
+    }
+    else{
+    let numberValue=document.getElementById(numberId).innerText;
+    let inputValue=document.getElementById("display-number").value;
+    document.getElementById("display-number").value=inputValue+ numberValue; 
+    }
+}
+function checkPinValidity(){
+    let inputValue=document.getElementById("display-number").value;
+    let pin=document.getElementById("display-pin").value;
+    let tryLeft=document.getElementById("try-left").innerText;
+    let tryLeftValue=parseInt(tryLeft);
+    if(tryLeftValue>0)
+    {
+        if(inputValue==pin){
+            document.getElementById("wrong-pin").style.display="none"
+            document.getElementById("pin-matched").style.display="block"
+            document.getElementById("try-left").innerText=3;
+            document.getElementById("try-left-text").style.display="none"    
+        }
+        else{
+            document.getElementById("pin-matched").style.display="none";
+            document.getElementById("wrong-pin").style.display="block"
+            document.getElementById("try-left-text").style.display="block"
+            document.getElementById("try-left").innerText=tryLeftValue-1;
+        }
+    }
+    else{
+        document.getElementById("submit-btn").disabled=true;
+    }
+}
